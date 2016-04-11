c9d2c1575171df0919cd9886f8b92b5934bb93df
Mahadi Shahadat Good <mahadishahadat@macbook.local>
2016-04-11 12:10:16 -0400

    OS X 10.11

--- src/main/java/com/medialets/CISauceDemo/BasePage.java
+++ src/main/java/com/medialets/CISauceDemo/BasePage.java
@@ -20,7 +20,7 @@ public class BasePage
 		if(browserName.equalsIgnoreCase("chrome"))
 		{
 			DesiredCapabilities caps = DesiredCapabilities.chrome();
-			caps.setCapability("platform", "Windows 10");
+			caps.setCapability("platform", "OS X 10.11");
 			caps.setCapability("version", "45.0");
 
 			driver = new RemoteWebDriver(new URL(URL), caps);
